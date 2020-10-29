3f2cd1ca05d46ac5078aa124fb1032158708e877
alexkutner <kutner@gmail.com>
2020-10-29 13:10:59 -0700

    WIP:simple concurrent train detection is implimented and working.  It's expensive as it rebuilds the set on every request

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -27,3 +27,62 @@ def get_list_of_routes():
     return {"routes": keys}
 
 
+@bp.route('/routes/next_concurrent_trains')
+def next_concurrent_trains():
+    time = request.args.get('time', '')
+    return_value = find_next_concurrent_trains(time)
+
+    return {'time': return_value}
+
+
+def find_next_concurrent_trains(time):
+    concurrent_train_times = build_concurrent_table()
+    start = 0
+    end = len(concurrent_train_times)
+    while start < end:
+        mid = int(start + (start + end) / 2)
+        if concurrent_train_times[mid] == time:
+            return time
+        elif concurrent_train_times[mid] < time:
+            start = mid + 1
+        else:
+            end = mid
+    return concurrent_train_times[mid]
+
+
+# return a sorted list of times where multiple trains will be in the station at the same time
+# The list is build by doing a sort of merge sort across all routes submitted
+def build_concurrent_table():
+    it = db.keys()
+    schedules = list()
+    for key in it:
+        schedules.append(db.fetch(key)['times'])
+
+    concurrent_list = []
+    while len(schedules) > 1:
+        popset = []
+        min_val = None
+        # move through sets looking for small non matches or groups of matches
+        for i in range(0, len(schedules)):
+            if not min_val or min_val > schedules[i][0]:
+                popset = [i]
+                min_val = schedules[i][0]
+            elif min_val == schedules[i][0]:
+                min_val == schedules[i][0]
+                popset.append(i)
+
+        # if we found a match copy it from the first schedule we saw it on
+        if len(popset) > 1:
+            concurrent_list.append(schedules[popset[0]][0])
+
+        # remove smallest items in list
+        deletion_adjuster = 0
+        for i in popset:
+            schedules[i-deletion_adjuster].pop(0)
+            if len(schedules[i-deletion_adjuster]) == 0:
+                schedules.pop(i-deletion_adjuster)
+                deletion_adjuster += 1
+
+        return concurrent_list
+
+
--- tests/test_flaskr.py
+++ tests/test_flaskr.py
@@ -34,7 +34,28 @@ def test_missing_route(client, app):
     response = client.get('/routes/H1B2')
     assert response.status_code == 404
 
+
 def test_add_route_with_long_name(client, app):
     response = client.post('/routes/a_name_longer_then_4_chars',
                            json={"times":["9:45 AM"]})
     assert response.status_code == 400
+
+
+def test_concurrent_routes(client, app):
+    response = client.post('/routes/H1B2',
+                           json={"times":["9:45 AM"]})
+    assert response.status_code == 201
+    response = client.post('/routes/J',
+                           json={"times":["9:45 AM"]})
+    assert response.status_code == 201
+
+    response = client.get('/routes/next_concurrent_trains?time=9:40AM')
+    assert response.status_code == 200
+    print(response.data)
+    data = json.loads(response.data)
+    assert data['time'] == '9:45 AM'
+
+
+# test bad formated times
+# test times out of order if they get sorted
+# test bad time or missing time arg for concurrent
